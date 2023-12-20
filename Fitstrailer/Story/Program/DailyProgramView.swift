1319b70f8a9e1fa7485b376c223702c6fd2d1591
Can Kaynar <cannkaynarr@outlook.com>
2023-12-21 00:32:34 +0300

    Added bodyParts as an enum
    Table improved.

---
+++ Fitstrailer/Story/Program/DailyProgramView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyProgramView.swift
+//  Fitstrailer
+//
+//  Created by Can on 21.12.2023.
+//
+
+import SwiftUI
+
+struct DailyProgramView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyProgramView()
+}
